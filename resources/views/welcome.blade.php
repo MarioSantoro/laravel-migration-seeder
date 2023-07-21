67800bb27f6514fce4fd1f7aa4224b12289d4281
MarioSantoro <mariosantoro124@gmail.com>
2023-07-21 14:49:24 +0200

    Created model Train and created controller Page and show on DOM the date od datebase

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,24 @@
 
 
 @section('main-content')
-    <h1>Benvenuti nel Main</h1>
+    <div class="container">
+        <div class="row">
+            @foreach ($trains as $train)
+                <div class="col-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="text-card">Nome Azienda : {{ $train->Azienda }}</p>
+                            <p class="text-card">Partenza : {{ $train->Stazione_Di_Partenza }}</p>
+                            <p class="text-card">Arrivo a : {{ $train->Stazione_Di_Arrivo }}</p>
+                            <p class="text-card">Orario Partenza : {{ $train->Orario_Di_Partenza }}</p>
+                            <p class="text-card">Orario di Arrivo : {{ $train->Orario_Di_Arrivo }}</p>
+                            <p class="text-card">Numero carrozze : {{ $train->Numero_Carrozze }}</p>
+                            <p class="text-card">In Orario : {{ $train->In_Orario }}</p>
+                            <p class="text-card">Cancellato : {{ $train->Cancellato }}</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
